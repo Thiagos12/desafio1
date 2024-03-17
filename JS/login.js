d3909c069270ae177e0af10c778cc69fd3cb5af2
Thiagos12 <thiagoserrazine@gmail.com>
2024-03-17 19:51:38 -0300

    ok commit

---
+++ JS/login.js
@@ -0,0 +1,62 @@
+const EMAIL = "admin@admin.com";
+const SENHA = "123456"
+
+let campoEmail = document.querySelector("#email");
+let campoSenha = document.querySelector("#senha");
+let btnentrar = document.getElementById('btn-entrar');
+
+btnentrar.addEventListener("click", () =>  {
+    // Capiturando os valores digitados pelo usuario
+    let emaildigitado = campoEmail.value.toLowerCase();
+    let senhadigitada = campoSenha.value;
+
+    autenticar(emaildigitado, senhadigitada);
+    
+});
+
+function autenticar (email, senha){
+    //Preciso saber url da API
+    const URL ='http://localhost:3400/login'
+
+    // Criar um request para API
+    fetch(URL, {
+        method : 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({email,senha}) 
+    })
+    // Se der certo, direcionar para a tela de home
+    .then(response => response = response.json())
+    .then( response => {
+        console.log(response)
+
+        if(!!response.mensagem){
+            alert(response.mensagem);
+            return;
+        }
+//mostrar loading
+        mostrarloading();
+
+        salvarToken(response.token);
+        salvarUsuario(response.usuario);
+
+        setTimeout(() => {
+            window.open('controle-produtos.html', '_self')
+        },3000)
+
+    })
+    // Se der errado, mandar mensagem para usuario
+    .catch(erro => {
+        console.log(erro)
+    })
+
+
+}
+
+function mostrarloading(){
+    // Capturar campo de loading e mostrar ele na tela
+    const divloading = document.getElementById("loading");
+    divloading.style.display = "block";
+    // pegar elemento caixa de login e esconder ela
+    const divmainlogin = document.querySelector('div.main-login');
+    divmainlogin.style.display = "none";
+}
---
+++ JS/loja.js
@@ -0,0 +1,73 @@
+const items = [
+    {
+        id: 0,
+        nome: 'Camiseta Branca Feminina',
+        img: './img/img-loja-fotos.jpg',
+        quantidade: 0,
+        valor: 19.99
+    },
+    {
+        id: 1,
+        nome: 'Short da Nike',
+        img: './img/short-nike.png',
+        quantidade: 0,
+        valor: 44.59
+    },
+    {
+        id: 2,
+        nome: 'Camiseta do Barcelona',
+        img: './img/camisa-barcelona.png',
+        quantidade: 0,
+        valor: 59.98
+    }
+];
+
+inicializarloja = () => {
+    var containerProdutos = document.getElementById('produtos')
+    items.map((val) => {
+        containerProdutos.innerHTML+= `
+
+        <div class="produto-single">
+        <img src="`+ val.img+`" />
+        <p>`+val.nome+`</p>
+        <a key="`+val.id+`" href="#" onclick="alert('Você adicionou ${val.nome} no Carrinho!')">Adicionar ao carrinho</a>
+        </div>
+
+        `;
+
+    })
+}
+
+inicializarloja();
+
+atualizarCarrinho = () => {
+    var containerCarrinho = document.getElementById('carrinho')
+    containerCarrinho.innerHTML = "";
+    items.map((val) => {
+        if(val.quantidade > 0){
+        containerCarrinho.innerHTML+= `
+        <div class="single-checkout">
+        <p style="float:left;">Produto: `+val.nome+`</p><br>
+        <p style="float:right;">Quantidade: `+val.quantidade+`</p><br><br>
+        <p style="float:right;">Valor un: `+val.valor+`</p>
+        <div style="clear:both"></div>
+        </div>
+
+        `;
+        }
+
+    })
+
+}
+
+var links = document.getElementsByTagName("a");
+
+for(var i = 0; i < links.length; i++) {
+    links[i].addEventListener( "click", function() {
+        let key = this.getAttribute('key');
+        items[key].quantidade++;
+        atualizarCarrinho();
+        return false;
+})
+
+};
